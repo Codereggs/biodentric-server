d03ae186b234a213118d270539425a4157eb1769
Esteban <estebangabrielgarciasanchez@gmail.com>
2024-09-13 23:04:24 -0300

    feat: winston logger

--- server/server-cron.js
+++ server/server-cron.js
@@ -1,21 +1,29 @@
-import { schedule } from "node-cron";
-import { fetchReviews } from "../pages/api/fetchReviews.js";
-import getReviews from "../pages/api/getReviews.js";
 import express from "express";
 import http from "http";
+import { schedule } from "node-cron";
+import winston from "winston";
+import { fetchReviews } from "../pages/api/fetchReviews.js";
+import getReviews from "../pages/api/getReviews.js";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.simple(),
+  transports: [new winston.transports.Console()],
+});
 
 // Init Express
 const app = express();
 
-//Execute all mondays at 00:00 (one time per week)
+/* //Execute all mondays at 00:00 (one time per week)
 schedule("0 0 * * 1", () => {
+  logger.info("Init task to find reviews");
+  fetchReviews();
+}); */
+
+schedule("*/10 * * * * *", () => {
+  logger.info("Init task to find reviews");
   fetchReviews();
 });
-
-//schedule("*/10 * * * * *", () => {
-//console.log("Iniciando tarea cada 30 segundos para obtener reseñas");
-//fetchReviews();
-//});
 
 // GET Endpoint
 app.get("/api/reviews", async (req, res) => {
