14109db98b19218544173c9cf970a13f2e8c4b12
Esteban <estebangabrielgarciasanchez@gmail.com>
2024-09-13 23:25:59 -0300

    fix: server cron

--- pages/api/server-cron.js
+++ pages/api/server-cron.js
@@ -7,7 +7,7 @@ schedule("0 0 * * 1", () => {
 }); */
 
 export default function handler(req, res) {
-  /*   logger.info("Init task to find reviews");
-  fetchReviews(); */
+  logger.info("Init task to find reviews");
+  fetchReviews();
   res.status(200).end("Hello Cron!");
 }
