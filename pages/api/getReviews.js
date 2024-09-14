40c3b1036f5639762be384d3fe0bc055b6942318
Esteban <estebangabrielgarciasanchez@gmail.com>
2024-09-13 22:46:24 -0300

    feat get reviews endpoint

--- pages/api/getReviews.js
+++ pages/api/getReviews.js
@@ -4,20 +4,16 @@ import {
 } from "../../mongoDB/mongoDBConnection.js";
 import { Review } from "../../mongoDB/mongoDBSchema.js";
 
-export default async function handler(req, res) {
-  if (req.method === "GET") {
-    try {
-      await connectMongoDB();
-      // Obtener todas las reviews de MongoDB
-      const reviews = await Review.find();
-      // Si hay reviews, devolverlas en formato JSON
-      await disconnectMongoDB();
-      res.status(200).json(reviews);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Error al obtener las reseñas" });
-    }
-  } else {
-    res.status(405).json({ message: "Método no permitido" });
+export default async function getReviews(req, res) {
+  try {
+    await connectMongoDB();
+    // Obtener todas las reviews de MongoDB
+    const reviews = await Review.find();
+    // Si hay reviews, devolverlas en formato JSON
+    await disconnectMongoDB();
+    return reviews;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
 }
--- server/server-cron.js
+++ server/server-cron.js
@@ -1,15 +1,37 @@
+import express from "express";
+import http from "http";
 import { schedule } from "node-cron";
 import { fetchReviews } from "../pages/api/fetchReviews.js";
-import { connectMongoDB } from "../mongoDB/mongoDBConnection.js";
+import getReviews from "../pages/api/getReviews.js";
 
-/*
+// Init Express
+const app = express();
+
 //Execute all mondays at 00:00 (one time per week)
 schedule("0 0 * * 1", () => {
   fetchReviews();
 });
- */
 
-schedule("*/10 * * * * *", () => {
-  console.log("Iniciando tarea cada 30 segundos para obtener reseñas");
-  fetchReviews();
+//schedule("*/10 * * * * *", () => {
+//console.log("Iniciando tarea cada 30 segundos para obtener reseñas");
+//fetchReviews();
+//});
+
+// GET Endpoint
+app.get("/api/reviews", async (req, res) => {
+  if (req.method === "GET") {
+    const request = await getReviews();
+    if (request !== null) {
+      res.status(200).json(request);
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+});
+
+// Init server
+const PORT = process.env.PORT || 443;
+
+http.createServer(app).listen(PORT, () => {
+  console.log(`Server connected port=${PORT}`);
 });
